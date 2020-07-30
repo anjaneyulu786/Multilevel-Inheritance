43b35e39cac8b02530cf5ab5c375c2fa42ad6ac3
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-07-30 15:12:02 +0530

    Update Multilevel.java

--- Multilevel.java
+++ Multilevel.java
@@ -1,16 +1,18 @@
-interface Printable{  
-void print();  
+interface Printable {  
+void print ();  
 }  
-interface Showable{  
+interface Showable {  
 void show();  
 }  
-class A7 implements Printable,Showable{  
-public void print(){System.out.println("Hello");}  
-public void show(){System.out.println("Welcome");}  
+class A7 implements Printable,Showable {  
+public void print () { System.out.println ("Hello");
+                   }  
+public void show () { System.out.println ("Welcome");
+                  }  
   
-public static void main(String args[]){  
+public static void main (String args[]) {  
 A7 obj = new A7();  
-obj.print();  
-obj.show();  
+obj.print ();  
+obj.show ();  
  }  
 }  
